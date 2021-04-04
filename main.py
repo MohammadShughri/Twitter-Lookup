45965127c5606f649a0b9cf43945e5c6fc180e02
Mohammad Shughri <mohammadws19@gmail.com>
2021-04-05 01:24:55 +0300

    Comments

--- main.py
+++ main.py
@@ -31,14 +31,14 @@ def extract_tweet_attributes(tweet_object):
     tweet_list = []
     # loop through tweet objects
     for tweet in tweet_object:
-        tweet_at = tweet.user.name
-        tweet_user = tweet.user.screen_name
-        tweet_id = tweet.id  # unique integer identifier for tweet
-        text = tweet.text  # utf-8 text of tweet
+        tweet_at = tweet.user.name # User's @ name/ID
+        tweet_user = tweet.user.screen_name # User's display name
+        tweet_id = tweet.id  # ID of the tweet
+        text = tweet.text  # Text of the tweet
         favorite_count = tweet.favorite_count
         retweet_count = tweet.retweet_count
         created_at = tweet.created_at  # utc time tweet created
-        reply_to_status = tweet.in_reply_to_screen_name  # if reply int of orginal tweet id
+        reply_to_status = tweet.in_reply_to_screen_name  # Names of users who replied
 
         # Adding tweet attributes to the list
         tweet_list.append({'tweet_id': tweet_id,
@@ -66,12 +66,12 @@ def extract_tweet_attributes(tweet_object):
 
 # Assigning the extract function with the search results to df
 df = extract_tweet_attributes(moeghri_tweets)
+sorted_df = df.sort_values(by=["favorite_count", "retweet_count", "created_at"], ascending=[False, False, False])
 
 # Putting the attributes in a CSV file
 x = pd.read_csv(r'C:\Users\Mohammad Shughri\Desktop\test2.csv')
 
 # Sorting the values in descending order
-sorted_df = df.sort_values(by=["favorite_count", "retweet_count", "created_at"], ascending=[False, False, False])
 sorted_df.to_csv('test2.csv', index=True)
 print(sorted_df)
 
