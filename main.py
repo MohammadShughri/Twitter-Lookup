539feec4fdad692c66883497dcb10e36254d667e
Mohammad Shughri <mohammadws19@gmail.com>
2021-04-05 09:56:12 +0300

    Comments and minor changes

--- main.py
+++ main.py
@@ -22,60 +22,61 @@ def connect_to_twitter_Auth():
 api = connect_to_twitter_Auth()
 
 # Finding a certain word (query)
-moeghri_tweets = api.search(q='request for startup', lang='en', count=80)
+tweet_search = api.search(q='request for startup', lang='en', count=50)
 
 
 # Extracting data from tweets
-def extract_tweet_attributes(tweet_object):
+def extract_tweet_attributes(tweet_obj):
     # Empty list
     tweet_list = []
     # loop through tweet objects
-    for tweet in tweet_object:
+    for tweet in tweet_obj:
         tweet_at = tweet.user.name # User's @ name/ID
         tweet_user = tweet.user.screen_name # User's display name
         tweet_id = tweet.id  # ID of the tweet
         text = tweet.text  # Text of the tweet
         favorite_count = tweet.favorite_count
         retweet_count = tweet.retweet_count
-        created_at = tweet.created_at  # utc time tweet created
-        reply_to_status = tweet.in_reply_to_screen_name  # Names of users who replied
+        tweet_date = tweet.created_at  # utc time tweet created
+        discussion_users = tweet.in_reply_to_screen_name  # @ Names of users who replied
 
         # Adding tweet attributes to the list
-        tweet_list.append({'tweet_id': tweet_id,
-                           'twitter_display_name': tweet_user,
-                           'tweet_@_name': tweet_at,
-                           'tweet_text': text,
-                           'favorite_count': favorite_count,
-                           'retweet_count': retweet_count,
-                           'created_at': created_at,
-                           'reply_to_status': reply_to_status,
+        tweet_list.append({'Tweet_id': tweet_id,
+                           'Twitter_display_name': tweet_user,
+                           'Tweet_@_name': tweet_at,
+                           'Tweet_text': text,
+                           'Favorite_count': favorite_count,
+                           'Retweet_count': retweet_count,
+                           'Date of the tweet': tweet_date,
+                           'Discussion': discussion_users,
                            })
 
     # Dataframe
-    df = pd.DataFrame(tweet_list, columns=['tweet_id',
-                                           'twitter_display_name',
-                                           'tweet_@_name',
-                                           'tweet_text',
-                                           'favorite_count',
-                                           'retweet_count',
-                                           'created_at',
-                                           'reply_to_status'])
+    df = pd.DataFrame(tweet_list, columns=['Tweet_id',
+                                           'Twitter_display_name',
+                                           'Tweet_@_name',
+                                           'Tweet_text',
+                                           'Favorite_count',
+                                           'Retweet_count',
+                                           'Date of the tweet',
+                                           'Discussion'])
 
     return df
 
 
 # Assigning the extract function with the search results to df
-df = extract_tweet_attributes(moeghri_tweets)
-sorted_df = df.sort_values(by=["favorite_count", "retweet_count", "created_at"], ascending=[False, False, False])
+df = extract_tweet_attributes(tweet_search)
+
 
 # Putting the attributes in a CSV file
 x = pd.read_csv(r'C:\Users\Mohammad Shughri\Desktop\test2.csv')
 
 # Sorting the values in descending order
-sorted_df.to_csv('test2.csv', index=True)
+sorted_df = df.sort_values(["Favorite_count", "Retweet_count", "Date of the tweet", "Discussion"], ascending=(False, False, False, False))
+sorted_df.to_csv(r'C:\Users\Mohammad Shughri\Desktop\test2.csv', index=False)
 print(sorted_df)
 
-# Outputing the CSV in an external webpage
+# Outputting the CSV in an external webpage
 a = pd.read_csv(r'C:\Users\Mohammad Shughri\Desktop\test2.csv')
 sorted_df.to_html("Table.html")
 html_file = sorted_df.to_html()
